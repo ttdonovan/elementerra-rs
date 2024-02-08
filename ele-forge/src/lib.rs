19ffdee671a17ab6d566149690cd75a81a86ffff
Tanner Donovan <ttdonovan@gmail.com>
2024-02-07 19:45:07 -0800

    add more elements and recipies

--- ele-forge/src/lib.rs
+++ ele-forge/src/lib.rs
@@ -17,12 +17,28 @@ pub enum Ele {
     Earth,
     Fire,
     Water,
-    Rain,
-    Heat,
     Pressure,
+    Rain,
+    Smoke,
+    Dust,
+    Fog,
+    Wind,
+    Heat,
     Life,
+    Energy,
+    Lava,
+    Steam,
+    Mud,
+    Time,
+    Glass,
+    Worm,
     Oil,
+    Sun,
+    Ocean,
+    Mountain,
     Metal,
+    Pond,
+    Seed,
     Plastic,
     Umbrella,
 }
@@ -34,10 +50,18 @@ impl Ele {
             Ele::Earth => Some(10000),
             Ele::Fire => Some(10000),
             Ele::Water => Some(10000),
-            Ele::Rain => Some(42000),
-            Ele::Heat => Some(42000),
             Ele::Pressure => Some(42000),
+            Ele::Rain => Some(42000),
+            Ele::Smoke => Some(42000),
+            Ele::Dust => Some(42000),
+            Ele::Fog => Some(42000),
+            Ele::Wind => Some(42000),
+            Ele::Heat => Some(42000),
             Ele::Life => Some(42000),
+            Ele::Energy => Some(42000),
+            Ele::Lava => Some(42000),
+            Ele::Steam => Some(42000),
+            Ele::Mud => Some(42000),
             _ => None,
         }
     }
@@ -60,12 +84,28 @@ impl fmt::Display for Ele {
             Ele::Earth => write!(f, "Earth"),
             Ele::Fire => write!(f, "Fire"),
             Ele::Water => write!(f, "Water"),
-            Ele::Rain => write!(f, "Rain"),
-            Ele::Heat => write!(f, "Heat"),
             Ele::Pressure => write!(f, "Pressure"),
+            Ele::Rain => write!(f, "Rain"),
+            Ele::Smoke => write!(f, "Smoke"),
+            Ele::Dust => write!(f, "Dust"),
+            Ele::Fog => write!(f, "Fog"),
+            Ele::Wind => write!(f, "Wind"),
+            Ele::Heat => write!(f, "Heat"),
             Ele::Life => write!(f, "Life"),
+            Ele::Energy => write!(f, "Energy"),
+            Ele::Lava => write!(f, "Lava"),
+            Ele::Steam => write!(f, "Steam"),
+            Ele::Mud => write!(f, "Mud"),
+            Ele::Time => write!(f, "Time"),
+            Ele::Glass => write!(f, "Glass"),
+            Ele::Worm => write!(f, "Worm"),
             Ele::Oil => write!(f, "Oil"),
+            Ele::Sun => write!(f, "Sun"),
+            Ele::Ocean => write!(f, "Ocean"),
+            Ele::Mountain => write!(f, "Mountain"),
             Ele::Metal => write!(f, "Metal"),
+            Ele::Pond => write!(f, "Pond"),
+            Ele::Seed => write!(f, "Seed"),
             Ele::Plastic => write!(f, "Plastic"),
             Ele::Umbrella => write!(f, "Umbrella"),
         }
@@ -81,12 +121,28 @@ impl str::FromStr for Ele {
             "Earth" => Ok(Ele::Earth),
             "Fire" => Ok(Ele::Fire),
             "Water" => Ok(Ele::Water),
-            "Rain" => Ok(Ele::Rain),
-            "Heat" => Ok(Ele::Heat),
             "Pressure" => Ok(Ele::Pressure),
+            "Rain" => Ok(Ele::Rain),
+            "Smoke" => Ok(Ele::Smoke),
+            "Dust" => Ok(Ele::Dust),
+            "Fog" => Ok(Ele::Fog),
+            "Wind" => Ok(Ele::Wind),
+            "Heat" => Ok(Ele::Heat),
             "Life" => Ok(Ele::Life),
+            "Energy" => Ok(Ele::Energy),
+            "Lava" => Ok(Ele::Lava),
+            "Steam" => Ok(Ele::Steam),
+            "Mud" => Ok(Ele::Mud),
+            "Time" => Ok(Ele::Time),
+            "Glass" => Ok(Ele::Glass),
+            "Worm" => Ok(Ele::Worm),
             "Oil" => Ok(Ele::Oil),
+            "Sun" => Ok(Ele::Sun),
+            "Ocean" => Ok(Ele::Ocean),
+            "Mountain" => Ok(Ele::Mountain),
             "Metal" => Ok(Ele::Metal),
+            "Pond" => Ok(Ele::Pond),
+            "Seed" => Ok(Ele::Seed),
             "Plastic" => Ok(Ele::Plastic),
             "Umbrella" => Ok(Ele::Umbrella),
             _ => Err(()),
@@ -111,12 +167,28 @@ impl ForgePrice for Recipe {
 
 #[derive(Debug)]
 enum Recipes {
-    Rain(Recipe),
     Pressure(Recipe),
-    Life(Recipe),
+    Rain(Recipe),
+    Smoke(Recipe),
+    Dust(Recipe),
+    Fog(Recipe),
+    Wind(Recipe),
     Heat(Recipe),
+    Life(Recipe),
+    Energy(Recipe),
+    Lava(Recipe),
+    Steam(Recipe),
+    Mud(Recipe),
+    Time(Recipe),
+    Glass(Recipe),
+    Worm(Recipe),
     Oil(Recipe),
+    Sun(Recipe),
+    Ocean(Recipe),
+    Mountain(Recipe),
     Metal(Recipe),
+    Pond(Recipe),
+    Seed(Recipe),
     Plastic(Recipe),
     Umbrella(Recipe),
 }
@@ -124,49 +196,96 @@ enum Recipes {
 impl fmt::Display for Recipes {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match self {
-            Recipes::Rain(_) => write!(f, "Rain"),
             Recipes::Pressure(_) => write!(f, "Pressure"),
-            Recipes::Life(_) => write!(f, "Life"),
+            Recipes::Rain(_) => write!(f, "Rain"),
+            Recipes::Smoke(_) => write!(f, "Smoke"),
+            Recipes::Dust(_) => write!(f, "Dust"),
+            Recipes::Fog(_) => write!(f, "Fog"),
+            Recipes::Wind(_) => write!(f, "Wind"),
             Recipes::Heat(_) => write!(f, "Heat"),
+            Recipes::Life(_) => write!(f, "Life"),
+            Recipes::Energy(_) => write!(f, "Energy"),
+            Recipes::Lava(_) => write!(f, "Lava"),
+            Recipes::Steam(_) => write!(f, "Steam"),
+            Recipes::Mud(_) => write!(f, "Mud"),
+            Recipes::Time(_) => write!(f, "Time"),
+            Recipes::Glass(_) => write!(f, "Glass"),
+            Recipes::Worm(_) => write!(f, "Worm"),
             Recipes::Oil(_) => write!(f, "Oil"),
+            Recipes::Sun(_) => write!(f, "Sun"),
+            Recipes::Ocean(_) => write!(f, "Ocean"),
+            Recipes::Mountain(_) => write!(f, "Mountain"),
             Recipes::Metal(_) => write!(f, "Metal"),
+            Recipes::Pond(_) => write!(f, "Pond"),
+            Recipes::Seed(_) => write!(f, "Seed"),
             Recipes::Plastic(_) => write!(f, "Plastic"),
             Recipes::Umbrella(_) => write!(f, "Umbrella"),
         }
     }
 }
 
-const FORGE_RECIPES: [Recipes; 8] = [
-    Recipes::Rain([Ele::Air, Ele::Air, Ele::Water, Ele::Water]),
+const FORGE_RECIPES: [Recipes; 24] = [
     Recipes::Pressure([Ele::Air, Ele::Earth, Ele::Earth, Ele::Earth]),
-    Recipes::Life([Ele::Air, Ele::Fire, Ele::Earth, Ele::Water]),
+    Recipes::Rain([Ele::Air, Ele::Air, Ele::Water, Ele::Water]),
+    Recipes::Smoke([Ele::Air, Ele::Fire, Ele::Earth, Ele::Fire]),
+    Recipes::Dust([Ele::Air, Ele::Air, Ele::Earth, Ele::Earth]),
+    Recipes::Fog([Ele::Air, Ele::Air, Ele::Water, Ele::Earth]),
+    Recipes::Wind([Ele::Air, Ele::Air, Ele::Air, Ele::Air]),
     Recipes::Heat([Ele::Fire, Ele::Air, Ele::Fire, Ele::Fire]),
+    Recipes::Life([Ele::Air, Ele::Fire, Ele::Earth, Ele::Water]),
+    Recipes::Energy([Ele::Air, Ele::Water, Ele::Water, Ele::Water]),
+    Recipes::Lava([Ele::Fire, Ele::Fire, Ele::Fire, Ele::Earth]),
+    Recipes::Steam([Ele::Water, Ele::Water, Ele::Fire, Ele::Water]),
+    Recipes::Mud([Ele::Water, Ele::Water, Ele::Earth, Ele::Earth]),
+    Recipes::Time([Ele::Dust, Ele::Dust, Ele::Dust, Ele::Dust]),
+    Recipes::Glass([Ele::Pressure, Ele::Heat, Ele::Earth, Ele::Fire]),
+    Recipes::Worm([Ele::Earth, Ele::Earth, Ele::Life, Ele::Mud]),
     Recipes::Oil([Ele::Pressure, Ele::Life, Ele::Life, Ele::Earth]),
+    Recipes::Sun([Ele::Fire, Ele::Energy, Ele::Lava, Ele::Heat]),
+    Recipes::Ocean([Ele::Water, Ele::Water, Ele::Water, Ele::Life]),
+    Recipes::Mountain([Ele::Earth, Ele::Energy, Ele::Earth, Ele::Earth]),
     Recipes::Metal([Ele::Heat, Ele::Heat, Ele::Pressure, Ele::Earth]),
+    Recipes::Pond([Ele::Earth, Ele::Rain, Ele::Water, Ele::Water]),
+    Recipes::Seed([Ele::Rain, Ele::Life, Ele::Earth, Ele::Earth]),
     Recipes::Plastic([Ele::Oil, Ele::Oil, Ele::Pressure, Ele::Heat]),
     Recipes::Umbrella([Ele::Plastic, Ele::Plastic, Ele::Rain, Ele::Metal]),
 ];
 
 pub struct Forge {
-    recipes: [Recipes; 8],
+    recipes: [Recipes; 24],
 }
 
 impl Forge {
     pub fn find_recipe(&self, name: &str) -> Option<&Recipe> {
         for recipe in &self.recipes {
             match recipe {
-                Recipes::Rain(_) if name == "Rain" => {
-                    if let Recipes::Rain(elements) = recipe {
-                        return Some(elements);
-                    }
-                }
                 Recipes::Pressure(_) if name == "Pressure" => {
                     if let Recipes::Pressure(elements) = recipe {
                         return Some(elements);
                     }
                 }
-                Recipes::Life(_) if name == "Life" => {
-                    if let Recipes::Life(elements) = recipe {
+                Recipes::Rain(_) if name == "Rain" => {
+                    if let Recipes::Rain(elements) = recipe {
+                        return Some(elements);
+                    }
+                }
+                Recipes::Smoke(_) if name == "Smoke" => {
+                    if let Recipes::Smoke(elements) = recipe {
+                        return Some(elements);
+                    }
+                }
+                Recipes::Dust(_) if name == "Dust" => {
+                    if let Recipes::Dust(elements) = recipe {
+                        return Some(elements);
+                    }
+                }
+                Recipes::Fog(_) if name == "Fog" => {
+                    if let Recipes::Fog(elements) = recipe {
+                        return Some(elements);
+                    }
+                }
+                Recipes::Wind(_) if name == "Wind" => {
+                    if let Recipes::Wind(elements) = recipe {
                         return Some(elements);
                     }
                 }
@@ -175,13 +294,78 @@ impl Forge {
                         return Some(elements);
                     }
                 }
+                Recipes::Life(_) if name == "Life" => {
+                    if let Recipes::Life(elements) = recipe {
+                        return Some(elements);
+                    }
+                }
+                Recipes::Energy(_) if name == "Energy" => {
+                    if let Recipes::Energy(elements) = recipe {
+                        return Some(elements);
+                    }
+                }
+                Recipes::Lava(_) if name == "Lava" => {
+                    if let Recipes::Lava(elements) = recipe {
+                        return Some(elements);
+                    }
+                }
+                Recipes::Steam(_) if name == "Steam" => {
+                    if let Recipes::Steam(elements) = recipe {
+                        return Some(elements);
+                    }
+                }
+                Recipes::Mud(_) if name == "Mud" => {
+                    if let Recipes::Mud(elements) = recipe {
+                        return Some(elements);
+                    }
+                }
+                Recipes::Time(_) if name == "Time" => {
+                    if let Recipes::Time(elements) = recipe {
+                        return Some(elements);
+                    }
+                }
+                Recipes::Glass(_) if name == "Glass" => {
+                    if let Recipes::Glass(elements) = recipe {
+                        return Some(elements);
+                    }
+                }
+                Recipes::Worm(_) if name == "Worm" => {
+                    if let Recipes::Worm(elements) = recipe {
+                        return Some(elements);
+                    }
+                }
                 Recipes::Oil(_) if name == "Oil" => {
                     if let Recipes::Oil(elements) = recipe {
                         return Some(elements);
                     }
                 }
+                Recipes::Sun(_) if name == "Sun" => {
+                    if let Recipes::Sun(elements) = recipe {
+                        return Some(elements);
+                    }
+                }
+                Recipes::Ocean(_) if name == "Ocean" => {
+                    if let Recipes::Ocean(elements) = recipe {
+                        return Some(elements);
+                    }
+                }
+                Recipes::Mountain(_) if name == "Mountain" => {
+                    if let Recipes::Mountain(elements) = recipe {
+                        return Some(elements);
+                    }
+                }
                 Recipes::Metal(_) if name == "Metal" => {
                     if let Recipes::Metal(elements) = recipe {
+                        return Some(elements);
+                    }
+                }
+                Recipes::Pond(_) if name == "Pond" => {
+                    if let Recipes::Pond(elements) = recipe {
+                        return Some(elements);
+                    }
+                }
+                Recipes::Seed(_) if name == "Seed" => {
+                    if let Recipes::Seed(elements) = recipe {
                         return Some(elements);
                     }
                 }
